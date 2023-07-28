2cb7018fabbc236e3c7a4f5ad8676f8d0f63e300
Timothy Batt <timothybatt3@gmail.com>
2023-07-28 12:35:49 -0400

    theme structure and functions created

---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php
+
+
+function timbatt_theme_support() {
+    // Automatic title support
+    add_theme_support("title-tag");
+}
+add_action("after_setup_theme", "timbatt_theme_support");
+
+function timbatt_register_styles() {
+    wp_enqueue_style(
+        "timbatt-style", 
+        get_template_directory_uri() . "/style.css",
+        array(),
+        wp_get_theme()->get("version"),
+        "all"
+    );
+}
+add_action("wp_enqueue_scripts", "timbatt_register_styles");
+
+
+
+function timbatt_register_scripts() {
+    wp_enqueue_script(
+        "timbatt-script",
+        get_template_directory_uri() . "/assets/js/app.js",
+        array(),
+        wp_get_theme()->get("version"),
+        "all",
+        true,
+    );
+}
+add_action("wp_enqueue_scripts", "timbatt_register_scripts");
+
+?>
